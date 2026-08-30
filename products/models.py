a3cd8fa3bbd7f0746c0eab187e641bce77eb19a0
pparsaamani2012 <p.parsa.amani.2.0.1.2@gmail.com>
2026-08-30 12:51:57 +0330

    Products models added 2 (edited)

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50,)
     description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), upload_to='categories/')
+    avatar = models.ImageField(_('avatar'),blank=True, upload_to='categories/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
@@ -18,14 +18,15 @@ class Category(models.Model):
         verbose_name = _("Category")
         verbose_name_plural = _("Categorys")
 
-
+    def __str__(self):
+        return self.title
 
 
 
 class Product(models.Model):
     title = models.CharField(_('title'), max_length=50,)
     description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), upload_to='products/')
+    avatar = models.ImageField(_('avatar'),blank=True, upload_to='products/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
@@ -36,6 +37,8 @@ class Product(models.Model):
         db_table='products'
         verbose_name = _("Product")
         verbose_name_plural = _("Products")
+        
+    
 
 
 class File(models.Model):
