721714b6248e7e8aec92cea12de16aca393b275e
pparsaamani2012 <p.parsa.amani.2.0.1.2@gmail.com>
2026-09-04 13:38:21 +0330

    First app finished

--- digtal_products/settings.py
+++ digtal_products/settings.py
@@ -12,24 +12,21 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+from .local_settings import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-l#hkysmnw=9ywa1p2%qh#_4zek4j+(r$541xqf1w*r!c53u3a='
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'rest_framework',
     'products',
 ]
 
@@ -72,8 +70,6 @@ WSGI_APPLICATION = 'digtal_products.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,7 +79,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -102,8 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -114,8 +107,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Media Files
--- products/models.py
+++ products/models.py
@@ -38,13 +38,24 @@ class Product(models.Model):
         verbose_name = _("Product")
         verbose_name_plural = _("Products")
         
-    
+    def __str__(self):
+        return self.title
 
 
 class File(models.Model):
-    product = models.ForeignKey('Product', verbose_name='product', on_delete=models.CASCADE)
+    FILE_AUDIO = 1
+    FILE_VIDEO = 2
+    FILE_PDF = 3
+    FILE_TYPES = (
+        (FILE_AUDIO, _('audio')),
+        (FILE_VIDEO, _('video')),
+        (FILE_PDF, _('pdf'))
+    )
+    
+    product = models.ForeignKey('Product', verbose_name='product', related_name='files', on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50,)
     description = models.TextField(_('description'), blank=True)
+    file_type = models.PositiveSmallIntegerField(_('file type'), choices=FILE_TYPES, default=FILE_VIDEO, blank=True)
     file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
@@ -56,3 +67,5 @@ class File(models.Model):
         verbose_name = _("File")
         verbose_name_plural = _("Files")
 
+    def __str__(self):
+        return self.title
